20b5ee32fd2494074b077738dd55bb4e9c56390c
JasmineGuy <guyjasmine.c@gmail.com>
2021-08-18 20:35:17 -0500

    added some delete button logic

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,35 @@
 import React from 'react'
+import ContentLoader, { Facebook } from 'react-content-loader'
 
-const Card = ({id, selected, length}) => {
-
+const Card = ({id, selected, length, pending}) => {
+    console.log('selected:', selected)
     return (
         <div className="card">
-            <div className="top">
-                <h1>{id}/{length}</h1>
-            </div>
-            <div className="title">
-                <p>{selected.name.first} {selected.name.last}</p>
-            </div>
-            <div className="main">
-                <h3>From: {selected.city}, {selected.country} </h3>
-                <h3>Job Title: {selected.title} </h3>
-                <h3>Employer: {selected.employer} </h3>
-            </div>
-            <div className="bottom">
-                <div>Favorite Movies:
-                        <p> 1. {selected.favoriteMovies[0]}</p>
-                        <p> 2. {selected.favoriteMovies[1]}</p>
-                        <p> 3. {selected.favoriteMovies[2]}</p>
+            {
+                pending || !selected ?
+                <ContentLoader />
+                :
+            <>
+                <div className="top">
+                    <h1>{id}/{length}</h1>
                 </div>
-            </div>
+                <div className="title">
+                    <p>{selected.name.first} {selected.name.last}</p>
+                </div>
+                <div className="main">
+                    <h3>From: {selected.city}, {selected.country} </h3>
+                    <h3>Job Title: {selected.title} </h3>
+                    <h3>Employer: {selected.employer} </h3>
+                </div>
+                <div className="bottom">
+                    <div>Favorite Movies:
+                            <p> 1. {selected.favoriteMovies[0]}</p>
+                            <p> 2. {selected.favoriteMovies[1]}</p>
+                            <p> 3. {selected.favoriteMovies[2]}</p>
+                    </div>
+                </div>
+            </>
+            }
         </div>
     )
 }
--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -3,12 +3,27 @@ import Card from './Card'
 import data from '../data'
 
 const CardContainer = () => {
-    const [id, setId] = useState(1)
-    const [selected, setSelected] = useState(data[0]);
-    const [length, setLength] = useState(data.length);
+    const [users, setUsers] = useState([]);
+    const [id, setId] = useState();
+    const [selected, setSelected] = useState();
+    const [length, setLength] = useState();
+    const [pending, setPending] = useState(false);
 
     const [prevDisabled, setPrevDisabled] = useState(false);
     const [nextDisabled, setNextDisabled] = useState(false);
+
+    useEffect(()=> {
+        setPending(true);
+        setTimeout(()=> {
+            setUsers(data)
+            setSelected(data[0])
+            setLength(data.length)
+            setId(data[0].id)
+            setPending(false)
+        }, 1000)
+    },[])
+
+    console.log('users:',users)
 
     useEffect(()=> {
         if(id ===1){
@@ -31,25 +46,38 @@ const CardContainer = () => {
 
     const findPrev = () => {
             setId(id - 1);
-            setSelected(data.find(user => user.id === id-1))
-            setLength(data.length)
+            setSelected(users.find(user => user.id === id-1))
+            setLength(users.length)
 
     }
 
     const findNext = () => {
         setId(id + 1);
-        setSelected(data.find(user => user.id === id+1))
-        setLength(data.length)
+        setSelected(users.find(user => user.id === id+1))
+        setLength(users.length)
+    }
+
+    const deleteUser =() => {
+        let usersCopy =[...users]
+        console.log('users-before', usersCopy)
+        let index = usersCopy.findIndex((user)=> user.id ===selected.id)
+        console.log('index:', index)
+        usersCopy.splice(index, 1)
+        console.log('users-after',usersCopy)
+        setUsers(usersCopy)
+        
     }
     return (
         <div className="card-container">
-            <Card id={id} selected={selected} length={length}/>
         
+            <Card pending={pending} id={id} selected={selected} length={length}/>
             <div className="button-container">
                 <button className="side-buttons" disabled={prevDisabled} onClick={findPrev}>Previous</button>
-                <button>Edit</button>
-                <button>Delete</button>
-                <button>New</button>
+                <section>
+                    <button className="crud-buttons">Edit</button>
+                    <button className="crud-buttons" onClick={deleteUser}>Delete</button>
+                    <button className="crud-buttons">New</button>
+                </section>
 
                 <button className="side-buttons" disabled={nextDisabled} onClick={findNext}>Next</button>
             </div>
